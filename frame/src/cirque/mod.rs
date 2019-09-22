40cfa212d7a37e57b08bdc598854782cd9835ec0
Veykril <lukastw97@gmail.com>
2019-09-22 16:29:36 +0200

    remove derivate from rendy-frame

--- frame/src/cirque/mod.rs
+++ frame/src/cirque/mod.rs
@@ -114,13 +114,23 @@ impl<'a, T, I, P> ReadyRef<'a, T, I, P> {
 /// Resource cirque.
 /// It simplifies using multiple resources
 /// when same resource cannot be used simulteneously.
-#[derive(Debug, derivative::Derivative)]
-#[derivative(Default(bound = ""))]
+#[derive(Debug)]
 pub struct Cirque<T, I = T, P = T> {
     pending: VecDeque<(P, usize, Frame)>,
     ready: VecDeque<(T, usize)>,
     marker: std::marker::PhantomData<fn() -> I>,
     counter: usize,
+}
+
+impl<T, I, P> Default for Cirque<T, I, P> {
+    fn default() -> Self {
+        Cirque {
+            pending: VecDeque::default(),
+            ready: VecDeque::default(),
+            marker: std::marker::PhantomData,
+            counter: usize::default(),
+        }
+    }
 }
 
 impl<T, I, P> Cirque<T, I, P> {
@@ -181,9 +191,17 @@ impl<T, I, P> Cirque<T, I, P> {
 /// Resource cirque that depends on another one.
 /// It relies on trusted ready index instead of frame indices.
 /// It guarantees to always return same resource for same index.
-#[derive(Debug, derivative::Derivative)]
-#[derivative(Default(bound = ""))]
+#[derive(Debug)]
 pub struct DependentCirque<T, I = T, P = T> {
     values: Vec<either::Either<T, P>>,
     marker: std::marker::PhantomData<fn() -> I>,
+}
+
+impl<T, I, P> Default for DependentCirque<T, I, P> {
+    fn default() -> Self {
+        DependentCirque {
+            values: Vec::default(),
+            marker: std::marker::PhantomData,
+        }
+    }
 }
