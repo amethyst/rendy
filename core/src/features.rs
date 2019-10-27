e026a49fe9939779af741ace92ee8ff171d67619
Zakarum <scareaangel@gmail.com>
2019-10-27 20:31:53 +0300

    Querying, matching and choosing backends Allow enabling all backend features

--- core/src/features.rs
+++ core/src/features.rs
@@ -1,3 +1,32 @@
+
+/// Resolve into input AST if dx12 backend is enabled.
+#[macro_export]
+#[cfg(all(feature = "dx12", all(target_os = "windows", not(target_arch = "wasm32"))))]
+macro_rules! rendy_with_dx12_backend {
+    ($($tt:tt)*) => { $($tt)* };
+}
+
+/// Resolve into input AST if dx12 backend is enabled.
+#[macro_export]
+#[cfg(not(all(feature = "dx12", all(target_os = "windows", not(target_arch = "wasm32")))))]
+macro_rules! rendy_with_dx12_backend {
+    ($($tt:tt)*) => {};
+}
+
+/// Resolve into input AST if dx12 backend is disabled.
+#[macro_export]
+#[cfg(not(all(feature = "dx12", all(target_os = "windows", not(target_arch = "wasm32")))))]
+macro_rules! rendy_without_dx12_backend {
+    ($($tt:tt)*) => { $($tt)* };
+}
+
+/// Resolve into input AST if dx12 backend is disabled.
+#[macro_export]
+#[cfg(all(feature = "dx12", all(target_os = "windows", not(target_arch = "wasm32"))))]
+macro_rules! rendy_without_dx12_backend {
+    ($($tt:tt)*) => {};
+}
+
 /// Resolve into input AST if empty backend is enabled.
 #[macro_export]
 #[cfg(feature = "gfx-backend-empty")]
@@ -26,88 +55,116 @@ macro_rules! rendy_without_empty_backend {
     ($($tt:tt)*) => {};
 }
 
-/// Resolve into input AST if dx12 backend is enabled.
+/// Resolve into input AST if gl backend is enabled.
 #[macro_export]
-#[cfg(feature = "gfx-backend-dx12")]
-macro_rules! rendy_with_dx12_backend {
+#[cfg(feature = "gfx-backend-gl")]
+macro_rules! rendy_with_gl_backend {
     ($($tt:tt)*) => { $($tt)* };
 }
 
-/// Resolve into input AST if dx12 backend is enabled.
+/// Resolve into input AST if gl backend is enabled.
 #[macro_export]
-#[cfg(not(feature = "gfx-backend-dx12"))]
-macro_rules! rendy_with_dx12_backend {
+#[cfg(not(feature = "gfx-backend-gl"))]
+macro_rules! rendy_with_gl_backend {
     ($($tt:tt)*) => {};
 }
 
-/// Resolve into input AST if dx12 backend is disabled.
+/// Resolve into input AST if gl backend is disabled.
 #[macro_export]
-#[cfg(not(feature = "gfx-backend-dx12"))]
-macro_rules! rendy_without_dx12_backend {
+#[cfg(not(feature = "gfx-backend-gl"))]
+macro_rules! rendy_without_gl_backend {
     ($($tt:tt)*) => { $($tt)* };
 }
 
-/// Resolve into input AST if dx12 backend is disabled.
+/// Resolve into input AST if gl backend is disabled.
 #[macro_export]
-#[cfg(feature = "gfx-backend-dx12")]
-macro_rules! rendy_without_dx12_backend {
+#[cfg(feature = "gfx-backend-gl")]
+macro_rules! rendy_without_gl_backend {
     ($($tt:tt)*) => {};
 }
 
 /// Resolve into input AST if metal backend is enabled.
 #[macro_export]
-#[cfg(feature = "gfx-backend-metal")]
+#[cfg(all(feature = "metal", any(all(target_os = "macos", not(target_arch = "wasm32"), all(target_arch = "aarch64", target_os = "ios")))))]
 macro_rules! rendy_with_metal_backend {
     ($($tt:tt)*) => { $($tt)* };
 }
 
 /// Resolve into input AST if metal backend is enabled.
 #[macro_export]
-#[cfg(not(feature = "gfx-backend-metal"))]
+#[cfg(not(all(feature = "metal", any(all(target_os = "macos", not(target_arch = "wasm32"), all(target_arch = "aarch64", target_os = "ios"))))))]
 macro_rules! rendy_with_metal_backend {
     ($($tt:tt)*) => {};
 }
 
 /// Resolve into input AST if metal backend is disabled.
 #[macro_export]
-#[cfg(not(feature = "gfx-backend-metal"))]
+#[cfg(not(all(feature = "metal", any(all(target_os = "macos", not(target_arch = "wasm32"), all(target_arch = "aarch64", target_os = "ios"))))))]
 macro_rules! rendy_without_metal_backend {
     ($($tt:tt)*) => { $($tt)* };
 }
 
 /// Resolve into input AST if metal backend is disabled.
 #[macro_export]
-#[cfg(feature = "gfx-backend-metal")]
+#[cfg(all(feature = "metal", any(all(target_os = "macos", not(target_arch = "wasm32"), all(target_arch = "aarch64", target_os = "ios")))))]
 macro_rules! rendy_without_metal_backend {
     ($($tt:tt)*) => {};
 }
 
 /// Resolve into input AST if vulkan backend is enabled.
 #[macro_export]
-#[cfg(feature = "gfx-backend-vulkan")]
+#[cfg(all(feature = "vulkan", all(any(target_os = "windows", all(unix, not(any(target_os = "macos", target_os = "ios")))), not(target_arch = "wasm32"))))]
 macro_rules! rendy_with_vulkan_backend {
     ($($tt:tt)*) => { $($tt)* };
 }
 
 /// Resolve into input AST if vulkan backend is enabled.
 #[macro_export]
-#[cfg(not(feature = "gfx-backend-vulkan"))]
+#[cfg(not(all(feature = "vulkan", all(any(target_os = "windows", all(unix, not(any(target_os = "macos", target_os = "ios")))), not(target_arch = "wasm32")))))]
 macro_rules! rendy_with_vulkan_backend {
     ($($tt:tt)*) => {};
 }
 
 /// Resolve into input AST if vulkan backend is disabled.
 #[macro_export]
-#[cfg(not(feature = "gfx-backend-vulkan"))]
+#[cfg(not(all(feature = "vulkan", all(any(target_os = "windows", all(unix, not(any(target_os = "macos", target_os = "ios")))), not(target_arch = "wasm32")))))]
 macro_rules! rendy_without_vulkan_backend {
     ($($tt:tt)*) => { $($tt)* };
 }
 
 /// Resolve into input AST if vulkan backend is disabled.
 #[macro_export]
-#[cfg(feature = "gfx-backend-vulkan")]
+#[cfg(all(feature = "vulkan", all(any(target_os = "windows", all(unix, not(any(target_os = "macos", target_os = "ios")))), not(target_arch = "wasm32"))))]
 macro_rules! rendy_without_vulkan_backend {
     ($($tt:tt)*) => {};
+}
+
+/// Resolve into input AST if target architecture is "wasm32"
+#[macro_export]
+#[cfg(target_arch = "wasm32")]
+macro_rules! rendy_wasm32 {
+    ($($tt:tt)*) => { $($tt)* };
+}
+
+/// Resolve into input AST if target architecture is "wasm32"
+#[macro_export]
+#[cfg(not(target_arch = "wasm32"))]
+macro_rules! rendy_wasm32 {
+    ($($tt:tt)*) => {};
+}
+
+/// Resolve into input AST if target architecture is not "wasm32"
+#[macro_export]
+#[cfg(target_arch = "wasm32")]
+macro_rules! rendy_not_wasm32 {
+    ($($tt:tt)*) => { };
+}
+
+/// Resolve into input AST if target architecture is not "wasm32"
+#[macro_export]
+#[cfg(not(target_arch = "wasm32"))]
+macro_rules! rendy_not_wasm32 {
+    ($($tt:tt)*) => { $($tt)* };
 }
 
 /// Resolve into input AST if rendy is requested to not perform slow safety checks.
